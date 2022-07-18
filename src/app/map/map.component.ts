2fdc9e3d50991e9a4104d1bda24dc8de18a41830
Carlos Planchart <carlos_planchart@live.com>
2022-07-18 14:21:57 +0200

    added json data

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -26,6 +26,74 @@ export class MapComponent implements AfterViewInit {
     popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
   });
 
+ 
+  /*
+  onMapReady(map: any) {
+   
+    // control that shows state info on hover
+    let info = new L.Control();
+    // here you want the reference to be info, therefore this = info
+    // so do not use es6 to access the class instance with this
+    info.onAdd = function (map: any) {
+      this._div = L.DomUtil.create('div', 'info');
+      this.update();
+      return this._div;
+    };
+    // also here you want the reference to be info, therefore this = info
+    // so do not use es6 to access the class instance with this
+    info.update = function (props: any) {
+      this._div.innerHTML = '<h4>US Population Density</h4>' + (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+        : 'Hover over a state');
+    };
+
+    info.addTo(map);
+
+    // get color depending on population density value
+    const getColor = d => {
+      return d > 1000 ? '#800026' :
+        d > 500 ? '#BD0026' :
+          d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+              d > 50 ? '#FD8D3C' :
+                d > 20 ? '#FEB24C' :
+                  d > 10 ? '#FED976' :
+                    '#FFEDA0';
+    }
+
+
+
+    let geojson;
+
+  
+
+
+    const legend = new L.Control({ position: 'bottomright' });
+
+    legend.onAdd =( map: any) => {
+
+      let div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [],
+        from, to;
+
+      for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+          '<i style="background:' + getColor(from + 1) + '"></i> ' +
+          from + (to ? '&ndash;' + to : '+'));
+      }
+
+      div.innerHTML = labels.join('<br>');
+      return div;
+    };
+
+    legend.addTo(map);
+  }
+  */
+
   highlightFeature = (e: any) => {
     const layer = e.target;
 
